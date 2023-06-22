a59737b2988bb73c35d26585e8a282331a02cd52
Yashdeep Raj <yashdeep.rajput019@gmail.com>
2023-06-22 20:29:25 +0530

    description fix

--- app/head.tsx
+++ app/head.tsx
@@ -3,11 +3,10 @@ export default function Head() {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="I'm Yashdeep Raj, full-stack developer based in Tundla, India. My journey in web development began during my first year of college." />
-      <meta name="keywords" content="yashdeep sable, portfolio, yashdeep, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta name="keywords" content="yashdeep raj, portfolio, yashdeep, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio, node, nuxt, vuejs, nest, react, mongodb, mysql, next android, ios, flutter, auedbaki, xamarin, aspnet aws, azure, certified" />
       <meta property="og:site_name" content="Yashdeep Raj's Portfolio" />
       <meta property="og:title" content="Portfolio | Yashdeep Raj - Full Stack Developer" />
-      <meta property="og:description"
-        content="I'm Yashdeep Raj, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
+      <meta property="og:description" content="I'm Yashdeep Raj, full-stack developer based in Tundla, India. My journey in web development began during my first year of college." />
       <meta property="og:url" content="https://dev.auedbaki.com" />
       <meta property="og:image" content="https://dev.auedbaki.com/mylogo.webp" />
       <link rel="canonical" href="https://dev.auedbaki.com" />
